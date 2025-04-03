b91f47bda5860608662824fcdd5e184c35e368b5
Fawas Ajani <G00413222@atu.ie>
2025-04-03 14:49:16 +0100

    Declaring a variable

--- src/app/Services/weather.service.spec.ts
+++ src/app/Services/weather.service.spec.ts
@@ -4,6 +4,7 @@ import { TestBed } from '@angular/core/testing';
 import { WeatherService } from './weather.service';
 // Describing the test suite for the WeatherService
 describe('WeatherService', () => {
+  // Declaring a variable 'service'
   let service: WeatherService;
 
   beforeEach(() => {
