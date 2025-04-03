9bf2cb415852b528fd7b8942e319626bd893cdf7
Fawas Ajani <G00413222@atu.ie>
2025-04-03 14:26:24 +0100

    imported modules

--- src/app/Services/weather.service.ts
+++ src/app/Services/weather.service.ts
@@ -1,3 +1,6 @@
+//imported Modules
+
+
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -13,14 +16,14 @@ export class WeatherService {
 
   constructor(private httpClient: HttpClient, private geolocation: Geolocation) { }
 
-  //GPS Options
+
   options = {
     timeout: 10000,
     enableHighAccuracy: true,
     maximumAge: 3600
   };
 
-  //Gets location
+  
   GetCurrentCoordinates(): any {
     this.geolocation.getCurrentPosition().then((resp) => {
       this.latitude = resp.coords.latitude;
@@ -32,7 +35,7 @@ export class WeatherService {
     });
   }
 
-  //Gets weather and inputs location
+
   GetWeatherData(latitude: number, longitude: number): Observable<any> {
     console.log(this.latitude, this.longitude);
     console.log('https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&units=metric&appid=18c63d63d9d7c4d0335b9c5a99c20482');
