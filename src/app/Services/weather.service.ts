288b411d856d91fe4ab9f0d39914fe1477e43afe
Fawas Ajani <G00413222@atu.ie>
2025-04-03 14:32:47 +0100

    Make a GET request to the OpenWeather API to get the weather data

--- src/app/Services/weather.service.ts
+++ src/app/Services/weather.service.ts
@@ -40,7 +40,7 @@ export class WeatherService {
   GetWeatherData(latitude: number, longitude: number): Observable<any> {
     console.log(this.latitude, this.longitude);
     console.log('https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&units=metric&appid=18c63d63d9d7c4d0335b9c5a99c20482');
-
+    // Make a GET request to the OpenWeather API to get the weather data
     return this.httpClient.get('https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&units=metric&appid=18c63d63d9d7c4d0335b9c5a99c20482');
   }
 }
