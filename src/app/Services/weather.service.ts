0d2b3ae88588d052999a0445ca6b817a0289b35f
Fawas Ajani <G00413222@atu.ie>
2025-04-03 14:28:58 +0100

    generated a api key using openweatherapp

--- src/app/Services/weather.service.ts
+++ src/app/Services/weather.service.ts
@@ -35,7 +35,7 @@ export class WeatherService {
     });
   }
 
-
+  // Method to fetch weather data from the OpenWeather API using the latitude and longitude
   GetWeatherData(latitude: number, longitude: number): Observable<any> {
     console.log(this.latitude, this.longitude);
     console.log('https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&units=metric&appid=18c63d63d9d7c4d0335b9c5a99c20482');
