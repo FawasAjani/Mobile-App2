b152f3a00052a07155d61c1903c8260ce23cc672
Fawas Ajani <G00413222@atu.ie>
2025-04-04 17:49:44 +0100

    Routes

--- src/app/calender/calender-routing.module.ts
+++ src/app/calender/calender-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { CalenderPage } from './calender.page';
-
+// Define the routes for this module
 const routes: Routes = [
   {
     path: '',
